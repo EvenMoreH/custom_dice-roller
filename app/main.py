b0556d8ab81b45a7e94a096c8f949876e2496deb
EvenMoreH <hubert.lewan@gmail.com>
2025-01-19 12:51:48 +0100

    ready for docker

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
 from fasthtml.common import * # type: ignore
 from fasthtml.common import (
-    Button, Html, Head, Body, Div, Title, Titled, Link, Meta, Script, Input, Form, Base, H1, Img, A,
-    Redirect, Response, Path
+    Button, Html, Head, Body, Div, Title, Link, Meta, Script, Form, H1,
 )
 import random
 
 # for Docker
-# app, rt = fast_app(static_path="static") # type: ignore
+app, rt = fast_app(static_path="static") # type: ignore
 
 # for local
-app, rt = fast_app(static_path="app/static") # type: ignore
+# app, rt = fast_app(static_path="app/static") # type: ignore
 
-# TODO:
-# 1. Add visual or text indicator what was clicked
-# 2. Add text what were each results for each die from the set
 
 @rt("/")
 def homepage(session):
-    multiplier = session.get("multiplier", None)
+    session["multiplier"] = 1
     result = session.get("result", "")
     session["to_all"] = 0
     return Html(
@@ -75,21 +71,21 @@ def homepage(session):
             Div(
                 Button("d3", id="d3", hx_get="/d3", hx_target="#result", type="submit", style="background: #23303d;"),
                 Button("d4", id="d4", hx_get="/d4", hx_target="#result", type="submit", style="background: #23303d;"),
-                Button("d5", id="d5", hx_get="/d5", hx_target="#result", type="submit", style="background: #23303d;"),
                 Button("d6", id="d6", hx_get="/d6", hx_target="#result", type="submit", style="background: #23303d;"),
+                Button("d8", id="d8", hx_get="/d8", hx_target="#result", type="submit", style="background: #23303d;"),
                 cls="grid-1x4"
             ),
             Div(
-                Button("d8", id="d8", hx_get="/d8", hx_target="#result", type="submit", style="background: #23303d;"),
                 Button("d10", id="d10", hx_get="/d10", hx_target="#result", type="submit", style="background: #23303d;"),
                 Button("d12", id="d12", hx_get="/d12", hx_target="#result", type="submit", style="background: #23303d;"),
                 Button("d20", id="d20", hx_get="/d20", hx_target="#result", type="submit", style="background: #23303d;"),
+                Button("d100", id="d100", hx_get="/d100", hx_target="#result", type="submit", style="background: #23303d;"),
                 cls="grid-1x4"
             ),
             # displaying results
             Div(
                 Div(style="padding: 10px;"),
-                Div("Result", id="result", style="font-size: 2rem; color: rgba(245, 245, 245, 0);"),
+                Div("Result", id="result", style="font-size: 1.6rem; color: rgba(245, 245, 245, 0);"),
                 Div(style="padding: 10px;"),
                 Div("Dice Result", id="dice", style="font-size: 1rem; height: 3rem; color: rgba(245, 245, 245, 0);"),
                 Div("Dice Result with Bonus", id="dice-w-bonus", style="font-size: 1rem; height: 3rem; color: rgba(245, 245, 245, 0);"),
@@ -115,6 +111,22 @@ def homepage(session):
                     Button("+10", id="p-ten", hx_get="/plus", hx_vals={"bonus": 10}, type="submit", hx_swap="none", style="background: #30422b;"),
                     cls="grid-1x5"
                 ),
+                Div(
+                    Button("+11", id="p-1one", hx_get="/plus", hx_vals={"bonus": 11}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+12", id="p-1two", hx_get="/plus", hx_vals={"bonus": 12}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+13", id="p-1three", hx_get="/plus", hx_vals={"bonus": 13}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+14", id="p-1four", hx_get="/plus", hx_vals={"bonus": 14}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+15", id="p-1five", hx_get="/plus", hx_vals={"bonus": 15}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    cls="grid-1x5"
+                ),
+                Div(
+                    Button("+16", id="p-1six", hx_get="/plus", hx_vals={"bonus": 16}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+17", id="p-1seven", hx_get="/plus", hx_vals={"bonus": 17}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+18", id="p-1eight", hx_get="/plus", hx_vals={"bonus": 18}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+19", id="p-1nine", hx_get="/plus", hx_vals={"bonus": 19}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    Button("+20", id="p-twenty", hx_get="/plus", hx_vals={"bonus": 20}, type="submit", hx_swap="none", style="background: #30422b;"),
+                    cls="grid-1x5"
+                ),
                 cls="container",
                 style="width: 100%; padding: 0; gap: 0;"
             ),
@@ -127,11 +139,11 @@ def homepage(session):
                     style="font-variant-caps: all-petite-caps;"
                 ),
             ),
-            Div("display checkbox text", id="checkbox-target", style="color:rgba(245, 245, 245, 0);"),
+            Div("display checkbox text", id="checkbox-target", style="font-size: 1.2rem; color:rgba(245, 245, 245, 0);"),
             # Reset button
-            Div(style="padding: 10px"),
+            Div(style="padding: 1px"),
             Div(
-                Button("Reset", hx_get="/", hx_target="body"),
+                Button("Reset", hx_get="/", hx_target="body", style="width: 120px"),
                 cls="container"
             )
         )
@@ -143,10 +155,10 @@ def toggle(each_die: bool, session):
     checkbox = "Adding to all dice" if each_die else "Adding once"
     if each_die is True:
         session["to_all"] = 1
-        return Div(checkbox, style="font-size: 1rem; font-variant-caps: all-petite-caps; color: rgba(0, 255, 20, 0.6);")
+        return Div(checkbox, style="font-size: 1.2rem; font-variant-caps: all-petite-caps; color: rgba(0, 255, 20, 0.6);")
     else:
         session["to_all"] = 0
-        return Div(checkbox, style="font-size: 1rem; font-variant-caps: all-petite-caps;")
+        return Div(checkbox, style="font-size: 1.2rem; font-variant-caps: all-petite-caps;")
 
 
 @rt("/select")
@@ -176,14 +188,14 @@ def plus(bonus:int, session):
 
             return Div(
                     f"{multiplier}{die_face} + ({multiplier}x{bonus}) [{result_added}]",
-                    hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
+                    hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
                     f"{dice_with_bonus}",
                     hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem; color: rgba(0, 255, 20, 0.6);")
         else:
             result_added = result + bonus
             return Div(
                     f"{multiplier}{die_face} +{bonus} [{result_added}]",
-                    hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
+                    hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
                     "",
                     hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
@@ -215,8 +227,10 @@ def d3(session):
     session["die_face"] = "d3"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d3 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d3 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
 @rt("/d4")
@@ -243,12 +257,14 @@ def d4(session):
     session["die_face"] = "d4"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d4 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d4 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
-@rt("/d5")
-def d5(session):
+@rt("/d100")
+def d100(session):
     multiplier = session.get("multiplier", None)
     if multiplier is None:
         return Div("Select a multiplier.", id="result")
@@ -257,7 +273,7 @@ def d5(session):
     total = 0
 
     for die in range(multiplier):
-        dice.append(random.randint(1, 5))
+        dice.append(random.randint(1, 100))
         print(dice)
 
     for die in dice:
@@ -268,11 +284,13 @@ def d5(session):
     dice.reverse()
 
     session["result"] = result
-    session["die_face"] = "d5"
+    session["die_face"] = "d100"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d5 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d100 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
 @rt("/d6")
@@ -299,8 +317,10 @@ def d6(session):
     session["die_face"] = "d6"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d6 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d6 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
 @rt("/d8")
@@ -327,8 +347,10 @@ def d8(session):
     session["die_face"] = "d8"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d8 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d8 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
 @rt("/d10")
@@ -355,8 +377,10 @@ def d10(session):
     session["die_face"] = "d10"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d10 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d10 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
 @rt("/d12")
@@ -383,8 +407,10 @@ def d12(session):
     session["die_face"] = "d12"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d12 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d12 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
 @rt("/d20")
@@ -411,12 +437,12 @@ def d20(session):
     session["die_face"] = "d20"
     session["dice_rolled"] = dice
 
-    return Div(f"{multiplier}d20 [{result}]", hx_swap_oob="true", id="result", style="font-size: 2rem;"), Div(
-        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;")
+    return Div(f"{multiplier}d20 [{result}]", hx_swap_oob="true", id="result", style="font-size: 1.6rem;"), Div(
+        f"{dice}", hx_swap_oob="true", id="dice", style="font-size: 1rem; height: 3rem;"), Div(
+                    "",
+                    hx_swap_oob="true", id="dice-w-bonus", style="font-size: 1rem; height: 3rem;")
 
 
-serve()
-
-# if __name__ == '__main__':
-#     # Important: Use host='0.0.0.0' to make the server accessible outside the container
-#     serve(host='0.0.0.0', port=5066) # type: ignore
+if __name__ == '__main__':
+    # Important: Use host='0.0.0.0' to make the server accessible outside the container
+    serve(host='0.0.0.0', port=5066) # type: ignore
